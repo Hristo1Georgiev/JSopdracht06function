1df4d1d385e2763411d818084b6d04c9d8bdfd92
Hristo Georgiev <83413781+Hristo1Georgiev@users.noreply.github.com>
2021-06-28 15:17:39 +0200

    Add files via upload
    Opdarcht 06 - Functions.

---
+++ JS06/script.js
@@ -0,0 +1,17 @@
+const pizzaMaking = function () {
+    console.log('Pizzabodem bereiden.')
+    console.log('Voeg toe de topings.')
+    console.log('Bak de pizza.')
+    console.log('Eet smakelijk!')
+};
+const sushiMaking = function () {
+    console.log('Rist bereiden.')
+    console.log('Voeg toe de topings.')
+    console.log('Rol de rijst in een sushi form.')
+    console.log('Eet smakelijk!')
+};
+sushiMaking();
+sushiMaking();
+sushiMaking();
+pizzaMaking();
+pizzaMaking();
